3ed1b2258858cf18566de36120bb512e522e0978
mostafa3mohamed3798-svg <mostafa3mohamed3798@gmail.com>
2026-08-06 17:27:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const price = document.getElementById("price");
+const signal = document.querySelector(".signal");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    price.innerText = "جاري التحليل...";
+    signal.innerText = "⏳ يتم تحليل السوق...";
+
+    setTimeout(() => {
+        price.innerText = "3392.50";
+        signal.innerText = "🟢 BUY\nقوة الإشارة: 91%";
+    }, 2000);
