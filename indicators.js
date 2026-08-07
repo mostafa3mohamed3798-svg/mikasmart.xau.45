6120478d19723f920b408e9e0205a8e0eee17ccd
mostafa3mohamed3798-svg <mostafa3mohamed3798@gmail.com>
2026-08-07 13:59:29 +0300

    Update indicators.js

--- indicators.js
+++ indicators.js
@@ -257,4 +257,9 @@ function generateProSignal(data, htfData = null, newsTimes = [], structureWindow
 }
 
 })(typeof window !== "undefined" ? window : globalThis);
-              
+// تصدير الدوال لتصبح متاحة لصفحة HTML
+window.TA = {
+    calculateEMA: calculateEMA,
+    calculateVWAP: calculateVWAP
+};
+
